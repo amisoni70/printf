5e87bca9aa815cf0771b06ccd48392e2cf71bf4a
sirln <siro@sirlawren.com>
2023-04-25 14:32:01 +0000

    Task 0

--- _printf.c
+++ _printf.c
@@ -13,42 +13,42 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int printed_chars = 0;
-	FormatSpecifier *spec = get_specifiers();
+	FormatSpecifier *spec;
 
-	if (format)
+	if (!format)
+		return (-1);
+
+	spec = get_specifiers();
+	va_start(args, format);
+	while (*format)
 	{
-		va_start(args, format);
-
-		while (*format)
+		if (*format == '%')
 		{
-			if (*format == '%')
-			{
-				format++;
-				while (spec->specifier)
-				{
-					if (spec->specifier == *format)
-					{
-						printed_chars += spec->print(args);
-						break;
-					}
-					spec++;
-				}
-				if (!spec->specifier)
-				{
-					printed_chars += write(1, "%", 1);
-					if (*format)
-					{
-						printed_chars += write(1, format, 1);
-					}
-				}
-			}
-			else
-			{
-				printed_chars += write(1, format, 1);
-			}
 			format++;
+			while (spec->specifier)
+			{
+				if (spec->specifier == *format)
+				{
+					printed_chars += spec->print(args);
+					break;
+				}
+				spec++;
+			}
+			if (!spec->specifier)
+			{
+				printed_chars += write(1, "%", 1);
+				if (*format)
+				{
+					printed_chars += write(1, format, 1);
+				}
+			}
 		}
-		va_end(args);
+		else
+		{
+			printed_chars += write(1, format, 1);
+		}
+		format++;
 	}
+	va_end(args);
 	return (printed_chars);
 }
