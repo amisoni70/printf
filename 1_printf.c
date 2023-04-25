77687af5f6e3fbc20d27cec0222081d97a3578c6
sirln <siro@sirlawren.com>
2023-04-25 17:33:16 +0000

    Task 1

--- 1_printf.c
+++ 1_printf.c
@@ -11,8 +11,14 @@ int print_integer(va_list args)
 	int n = va_arg(args, int);
 	int sign = 1;
 	char *buffer;
-	int i = 0;
+	int i = 0, j;
 	int printed_chars = 0;
+
+	if (n == 0)
+	{
+		printed_chars += write(1, "0", 1);
+		return (printed_chars);
+	}
 
 	if (n < 0)
 	{
@@ -20,10 +26,10 @@ int print_integer(va_list args)
 		n = -n;
 	}
 	/* Maximum size for a 64-bit integer is 20 */
-	buffer = malloc(sizeof(char) * 20);
+	buffer = malloc(sizeof(char) * 21);
 	if (!buffer)
 		return (-1);
-	while (n > 0)
+	while (n > 0 && i < 21)
 	{
 		buffer[i++] = n % 10 + '0';
 		n /= 10;
@@ -32,8 +38,11 @@ int print_integer(va_list args)
 	if (sign == -1)
 		buffer[i++] = '-';
 
-	while (--i >= 0)
-		printed_chars += write(1, &buffer[i], 1);
+	/*while (--i >= 0)*/
+		/*printed_chars += write(1, &buffer[i], 1);*/
+
+	for (j = i - 1; j >= 0; j--)
+		printed_chars += write(1, &buffer[j], 1);
 
 	free(buffer);
 	return (printed_chars);
