d8518ac49d0cb564e8cc2f8eb8b42e14b426d2ea
sirln <siro@sirlawren.com>
2023-04-25 23:57:47 +0000

    Task 3

--- 3_printf.c
+++ 3_printf.c
@@ -12,7 +12,7 @@ void write_with_buffer(int fd, const char *msg)
 	char buffer[BUFFER_SIZE];
 	int l = 0, n = 0, p = 0, c, bytes_written;
 
-	if (*msg == '\0')
+	if (!msg || *msg == '\0')
 		return;
 
 	while (msg[l] != '\0')
@@ -21,12 +21,16 @@ void write_with_buffer(int fd, const char *msg)
 	while (p < l)
 	{
 		c = (l - p) < BUFFER_SIZE ? (l - p) : BUFFER_SIZE;
+
 		for (; n < c; n++)
 			buffer[n] = msg[p + n];
-
-		bytes_written = write(fd, buffer, c);
-		if (bytes_written == -1)
-			return;
-		p += bytes_written;
+		while (c > 0)
+		{
+			bytes_written = write(fd, buffer, c);
+			if (bytes_written <= 0)
+				return;
+			c -= bytes_written;
+			p += bytes_written;
+		}
 	}
 }
