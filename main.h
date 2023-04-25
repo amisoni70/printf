151dccbd2b7687e3e20612afc66ca5b0a79676ad
amisoni70 <amisoni7014@gmail.com>
2023-04-25 05:42:36 -0700

    Task 0

--- main.h
+++ main.h
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-#ifndef MAIN_HEADER
-#define MAIN_HEADER
-=======
 #ifndef MAIN_H
 #define MAIN_H
->>>>>>> ami
 
 #include <stdlib.h>
 #include <stdarg.h>
@@ -12,19 +7,10 @@
 #include <string.h>
 #include <unistd.h>
 
-<<<<<<< HEAD
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_character(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
-int print_integer(va_list args);
-=======
 int _printf(const char *format, ...);
 int print_character(va_list ap);
 int print_percentage(__attribute__((unused))va_list ap);
 int print_string(va_list ap);
->>>>>>> ami
 
 /**
   *struct specifier - structure to handle specifier
