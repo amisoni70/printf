88b24dce711e2db054991a3dd3f106a26071f8be
amisoni70 <amisoni7014@gmail.com>
2023-04-25 05:20:48 -0700

    Task 0

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int print_character(va_list ap);
+int print_percentage(__attribute__((unused))va_list ap);
+int print_string(va_list ap);
+
+/**
+  *struct specifier - structure to handle specifier
+  *@specifier: specifier
+  *@print: function to print specifier
+  *
+  *Description: FormatSpecifier structure
+  */
+typedef struct specifier
+{
+	char specifier;
+	int (*print)(va_list);
+} FormatSpecifier;
+
+FormatSpecifier *get_specifiers();
+
+#endif
